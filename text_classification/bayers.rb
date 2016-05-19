9e58c05b726efe81c02beb4c7c964f88c3359c53
Alex Wang <ice_b0und@hotmail.com>
2016-05-19 17:26:44 +0800

    beautify scripts

--- text_classification/bayers.rb
+++ text_classification/bayers.rb
@@ -1,22 +1,18 @@
 require 'json'
 
-data_path = 'data'
-@total_words = 0
-@type_word_count = {}
-@doc_types = {}
+DATA_PATH = 'data'
 
-def str_to_words(str)
-  (str.each_char.map do |x|
+def file_to_words(file)
+  (File.read(file).force_encoding('iso-8859-1').split("\n\n")[1..-1].join.each_char.map do |x|
     c = x.downcase
     n, a, z = (c + 'az').unpack('C*')
     (a <= n && n <= z) ? c : ' '
   end).join.split.reject {|x| x.size <= 3 }
 end
 
-def calc_words(s, words)
-  str = s.force_encoding('iso-8859-1').split("\n\n")[1..-1].join
+def calc_words_from_file(file, words)
   n = 0
-  str_to_words(str).each do |x|
+  file_to_words(file).each do |x|
     words[x] += 1
     @total_words += 1
     n += 1
@@ -25,16 +21,18 @@ def calc_words(s, words)
 end
 
 if ARGV[0] == 'init'
-  `ls #{data_path}`.split.each do |dir|
+  @total_words = 0
+  @type_word_count = {}
+  @doc_types = {}
+  `ls #{DATA_PATH}`.split.each do |dir|
     type = dir.split('_').first
     @doc_types[type] = Hash.new(0)
     @type_word_count[type] = 0
     # 取每类前一半作为训练集
-    files = `find #{File.join(data_path, type)}* -type f`.split("\n")
+    files = `find #{File.join(DATA_PATH, type)}* -type f`.split("\n")
     files.first(files.size / 2).each do |file|
       puts file
-      str = File.read(file)
-      @type_word_count[type] += calc_words(str, @doc_types[type])
+      @type_word_count[type] += calc_words_from_file(file, @doc_types[type])
     end
   end
   json = {
@@ -51,21 +49,21 @@ else
   @correct = 0
   @total = 0
 
-  `ls #{data_path}`.split.each do |dir|
+  `ls #{DATA_PATH}`.split.each do |dir|
     doc_type = dir.split('_').first
     # 取每一类后一半作为测试集
-    files = `find #{File.join(data_path, doc_type)}* -type f`.split("\n")
+    files = `find #{File.join(DATA_PATH, doc_type)}* -type f`.split("\n")
     files.last(files.size).each do |file|
-      str = File.read(file).force_encoding('iso-8859-1').split("\n\n")[1..-1].join
       @values = {}
+      words_in_file = file_to_words(file)
       @doc_types.each do |type, words|
         val = Math.log(@type_word_count[type].to_f / @total_words)
-        str_to_words(str).each do |word|
+        words_in_file.each do |word|
           val += Math.log(((words[word] || 0) + 1).to_f / (@total_words + @type_word_count[type]))
         end
         @values[type] = val
       end
-      max_type = (@values.sort_by { |_type, val| val }).last.first
+      max_type = (@values.sort_by { |_, val| val }).last.first
       if max_type == doc_type
         @correct += 1
       else
